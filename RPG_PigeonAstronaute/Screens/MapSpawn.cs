fc9f97213b5b11dbbd342597e8d68541ca117e9b
Mathéo Da Silva <Matheo.Da-Silva@etu.univ-savoie.fr>
2022-01-14 11:39:30 +0100

    Ajoutez des fichiers projet.

---
+++ RPG_PigeonAstronaute/Screens/MapSpawn.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using System;
+using System.Collections.Generic;
+
+namespace RPG_PigeonAstronaute.Screens
+{
+    public class MapSpawn : Map
+    {
+        public Map map;
+        public TiledMapRenderer renduMap;
+
+        public MapSpawn(Game1 game) : base(game)
+        {
+            _mapName = "MapEtage";
+            _layerNames = new string[] { "Mur","Porte" };
+            _renduMap = renduMap;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ RPG_PigeonAstronaute/States/GameState.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using RPG_PigeonAstronaute.Screens;
+using System.Linq;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Content;
+
+namespace RPG_PigeonAstronaute.States
+{
+    public class GameState : State
+    {
+        private Game1 _game;
+        private MapSpawn mapSpawn;
+
+        private AnimatedSprite _perso;
+
+        private SpriteFont _font;
+
+        private List<Sprite> _sprites;
+
+        public int PlayerCount;
+
+        public GameState(Game1 game, ContentManager content) : base(game, content)
+        {
+            _game = game;
+            mapSpawn = new MapSpawn(_game);
+        }
+
+        public override void LoadContent()
+        {
+            mapSpawn.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                _game.ChangeState(new MenuState(_game, _content));
+
+            mapSpawn.Update(gameTime);
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            mapSpawn.Draw(gameTime);
+            mapSpawn._renduMap.Draw();
+            spriteBatch.End();
+        }
+    }
+}
