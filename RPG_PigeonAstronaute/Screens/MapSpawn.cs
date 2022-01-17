a07b34b19c0bb3df08c50c93c9551ff5fa7ff70b
clem.trompier@gmail.com <clem.trompier@gmail.com>
2022-01-17 17:34:07 +0100

    Ajoutez des fichiers projet.

---
+++ RPG_PigeonAstronaute/Screens/MapSpawn.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended.ViewportAdapters;
+using RPG_PigeonAstronaute.Sprites;
+using System.Collections.Generic;
+
+namespace RPG_PigeonAstronaute.Screens
+{
+    public class MapSpawn : Map
+    {
+        private readonly Game1 _game;
+        private SpriteBatch _spriteBatch;
+        private Vector2 _mcPosition;
+        private AnimatedSprite _mc;
+        private string animation;
+        public int vitesse;
+        private Mouvement mouvement;
+        public TiledMap map;
+        public TiledMapRenderer renduMap;
+        private TiledMapTileLayer _tpPoints;
+        private OrthographicCamera _camera;
+        private List<string> animations;
+
+        public MapSpawn(Game1 game) : base(game)
+        {
+            _game = game;
+            _mapName = "MapEtage";
+            collisionLayers = new string[] { "Mur", "Porte" };
+            _renduMap = renduMap;
+            animations = new List<string>() {
+                "IdleNorth", "IdleSouth", "IdleEast", "IdleWest",
+                "WalkNorth", "WalkSouth", "WalkEast", "WalkWest",
+                "StrikeNorth", "StrikeSouth", "StrikeEast", "StrikeWest",
+                "Dead"};
+        }
+
+        public override void Initialize()
+        {
+            _mcPosition = new Vector2(300, 300);
+            vitesse = 100;
+            animation = animations[5];
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            var viewportadapter = new BoxingViewportAdapter(_game.Window, GraphicsDevice, 800, 600);
+            _camera = new OrthographicCamera(viewportadapter);
+            mouvement = new Mouvement();
+        }
+
+        public override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("PersoBase.sf", new JsonContentLoader());
+            map = Content.Load<TiledMap>(_mapName);
+            _renduMap = new TiledMapRenderer(GraphicsDevice, map);
+            _mc = new AnimatedSprite(spriteSheet);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _renduMap.Update(gameTime);
+            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float walkSpeed = deltaSeconds * vitesse;
+            mouvement.Move(ref _mcPosition, ref animation, map, walkSpeed, 600, 800, _mc, animations, "Mur", "Porte");
+            _camera.LookAt(_mcPosition);
+            _mc.Play(animation);
+            _mc.Update(deltaSeconds);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            _renduMap.Draw(_camera.GetViewMatrix());
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_mc, new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2), 0, new Vector2((float)1.5, (float)1.5));
+            _spriteBatch.End();
+            _renduMap.Draw(11, _camera.GetViewMatrix());
+            _renduMap.Draw(15, _camera.GetViewMatrix());
+        }
+    }
+}
---
+++ RPG_PigeonAstronaute/States/GameState.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using RPG_PigeonAstronaute.Screens;
+using RPG_PigeonAstronaute.Sprites;
+using RPG_PigeonAstronaute.Controls;
+using MonoGame.Extended;
+using MonoGame.Extended.ViewportAdapters;
+using static RPG_PigeonAstronaute.Sprites.Perso;
+
+namespace RPG_PigeonAstronaute.States
+{
+    public class GameState : State
+    {
+        public MapSpawn mapSpawn;
+        private Vector2 _spawnPos = new Vector2(750, 750);
+
+        public GameState(Game1 game, ContentManager content) : base(game, content)
+        {
+            _game = game;
+            mapSpawn = new MapSpawn(_game);
+        }
+
+        public override void LoadContent()
+        {
+            mapSpawn.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                _game.ChangeState(new MenuState(_game, _content));
+
+            mapSpawn.Update(gameTime);
+            mapSpawn._renduMap.Update(gameTime);
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            mapSpawn.Draw(gameTime);
+            spriteBatch.End();
+        }
+    }
+}
